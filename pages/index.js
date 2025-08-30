e3d9f3fb2473671340a01363c2b902a4ea08d803
Germano Manoel De Melo <germanomanoel007@gmail.com>
2025-08-30 20:11:46 -0300

    adiciona um dos arquivos de manifesto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Teste, my name is Germano!</h1>
+    return <h1>Germano, não desista! Deus tem planos na tua vida! Você vai conseguir se tornar um programador EXCELENTE e trabalhar para grandes BIG TECHS.</h1>
 }
 
 export default Home;
