8e289a71815c4c2a35960adba53dbf3fd92dcfd5
Germano Manoel De Melo <germanomanoel007@gmail.com>
2025-08-31 18:15:44 -0300

    adicionando uma frase de efeito no nosso arquivo page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Germano, não desista! Deus tem planos na tua vida! Você vai conseguir se tornar um programador EXCELENTE e trabalhar para grandes BIG TECHS.</h1>
+    return <h1>Germano, não desista! Deus tem planos na tua vida! Você vai conseguir se tornar um programador EXCELENTE e trabalhar para grandes BIG TECHS. Tenha FÉ EM DEUS!!!</h1>
 }
 
 export default Home;
