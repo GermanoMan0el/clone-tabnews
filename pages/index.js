ba6a187dcbbf93b083f5f27f61c884ffdc0875bb
Germano Manoel De Melo <germanomanoel007@gmail.com>
2025-09-07 16:01:15 -0300

    mofica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Germano, não desista! Deus tem planos na tua vida! Você vai conseguir se tornar um programador EXCELENTE e trabalhar para grandes BIG TECHS. Tenha FÉ EM DEUS!!!</h1>
+    return <h1>Germano, não desista! Deus tem planos na tua vida! Você vai conseguir se tornar um programador EXCELENTE e trabalhar para grandes BIG TECHS. Tenha FÉ EM DEUS!!! 🙏🙏🙏</h1>
 }
 
 export default Home;
