364cbd8ab7f3f06c048efa116d5324657ef5e36a
Germano Manoel De Melo <germanomanoel007@gmail.com>
2025-09-07 16:15:27 -0300

    Adicionando estilo a pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Germano, não desista! Deus tem planos na tua vida! Você vai conseguir se tornar um programador EXCELENTE e trabalhar para grandes BIG TECHS. Tenha FÉ EM DEUS!!! 🙏🙏🙏</h1>
+    return(
+        <div style={{ fontFamily:'serif', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', textAlign: 'center', color:'white',  backgroundColor: 'black', borderRadius:'10px'}}>
+            <h1 style={{border:'1px solid black', padding:'10px', }}>Germano, não desista! Deus tem planos na sua vida! Você vai conseguir se tornar um programador EXCELENTE e trabalhar para grandes BIG TECHS. Tenha FÉ EM DEUS!!! 🙏🙏🙏</h1>
+        </div>
+    )
 }
 
 export default Home;
