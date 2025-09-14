3439fd0270d169aa9ee9554bee288d117c848ee7
Germano Manoel De Melo <germanomanoel007@gmail.com>
2025-09-14 16:39:29 -0300

    adiciona scripts `lint:check` e lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
 function Home() {
-   return(
-      <div 
-         style={{ fontFamily:'serif', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', textAlign: 'center', color:'white',  backgroundColor: 'black', borderRadius:'10px'}}
+   return (
+      <div
+         style={{
+            fontFamily: "serif",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            textAlign: "center",
+            color: "white",
+            backgroundColor: "black",
+            borderRadius: "10px",
+         }}
       >
-         <h1 style={{border:'1px solid black', padding:'10px', }}>
-               Gostaria de dizer a você que está lendo, que, não existe montanhas sem subida e caso você queira chegar ao top (seu objetivo), você precisa escalá-la e junto, vencer os desafios que estão por vir. Não desista, tenha FÉ e siga em frente!
+         <h1 style={{ border: "1px solid black", padding: "10px" }}>
+            Gostaria de dizer a você que está lendo, que, não existe montanhas
+            sem subida e caso você queira chegar ao top (seu objetivo), você
+            precisa escalá-la e junto, vencer os desafios que estão por vir. Não
+            desista, tenha FÉ e siga em frente!
          </h1>
       </div>
-   )
+   );
+}
+
+function teste() {
+   console.log("Teste");
+}
+
+function teste2() {
+   console.log("Hello World");
 }
 
 export default Home;
